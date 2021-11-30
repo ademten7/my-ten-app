cf319d0da5510c4a6e9ded22d2fe6bff7f636dd4
ademten82 <ademten7@gmail.com>
2021-11-30 13:20:28 +0100

    created app

---
+++ main.js
@@ -0,0 +1,10 @@
+//fixed bug
+function addition(a, b) {
+  console.log(`total of the two numbers are: ${a + b}`);
+}
+
+//add new feature
+function multiply(a, b) {
+  console.log(`multiply two numbers ${a * b}`);
+}
+module.exports = { addition, multiply };
